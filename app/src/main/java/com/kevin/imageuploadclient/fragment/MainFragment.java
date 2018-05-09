02a4c1288633dcacbcaea5fdfd8afc1b489117b1
develop-ddl <997004866@qq.com>
2018-05-09 11:41:20 +0800

    新增轮播图

--- app/src/main/java/com/kevin/imageuploadclient/fragment/MainFragment.java
+++ app/src/main/java/com/kevin/imageuploadclient/fragment/MainFragment.java
@@ -1,6 +1,7 @@
 package com.kevin.imageuploadclient.fragment;
 
 import android.content.ClipData;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -18,8 +19,15 @@ import com.bumptech.glide.Glide;
 import com.kevin.imageuploadclient.R;
 import com.kevin.imageuploadclient.fragment.basic.PictureSelectFragment;
 import com.kevin.imageuploadclient.util.Constant;
+import com.kevin.imageuploadclient.util.GlideImageLoader;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import okhttp3.Headers;
@@ -30,7 +38,10 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class MainFragment extends PictureSelectFragment {
+public class MainFragment extends PictureSelectFragment implements OnBannerListener {
+
+    private ArrayList<String> list_path;
+    private ArrayList<String> list_title;
 
     /** Toolbar */
     @Bind(R.id.toolbar)
@@ -53,6 +64,9 @@ public class MainFragment extends PictureSelectFragment {
 
     @Bind(R.id.bottom_navigation)
     BottomNavigationView buttomNavigationView;
+
+    @Bind(R.id.banner)
+    Banner mBanner;
 
 
     public static MainFragment newInstance() {
@@ -118,6 +132,63 @@ public class MainFragment extends PictureSelectFragment {
 
         buttomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        /*
+        //设置图片加载器
+        mBanner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        mBanner.setImages(images);
+        //banner设置方法全部调用完毕时最后调用
+        mBanner.start();
+        */
+
+        //放图片地址的集合
+        list_path = new ArrayList<>();
+        //放标题的集合
+        list_title = new ArrayList<>();
+
+        list_path.add(Constant.BASE_URL+"/files/banner/main1.png");
+        list_path.add(Constant.BASE_URL+"/files/banner/main2.png");
+        list_path.add(Constant.BASE_URL+"/files/banner/main3.png");
+        list_path.add(Constant.BASE_URL+"/files/banner/main4.png");
+        list_title.add("残损修复");
+        list_title.add("笔迹鉴定");
+        list_title.add("风格模仿");
+        list_title.add("手写识别");
+        //设置内置样式，共有六种可以点入方法内逐一体验使用。
+        mBanner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
+        //设置图片加载器，图片加载器在下方
+        mBanner.setImageLoader(new MyLoader());
+        //设置图片网址或地址的集合
+        mBanner.setImages(list_path);
+        //设置轮播的动画效果，内含多种特效，可点入方法内查找后内逐一体验
+        mBanner.setBannerAnimation(Transformer.Default);
+        //设置轮播图的标题集合
+        mBanner.setBannerTitles(list_title);
+        //设置轮播间隔时间
+        mBanner.setDelayTime(3000);
+        //设置是否为自动轮播，默认是“是”。
+        mBanner.isAutoPlay(true);
+        //设置指示器的位置，小点点，左中右。
+        mBanner.setIndicatorGravity(BannerConfig.CENTER)
+                //以上内容都可写成链式布局，这是轮播图的监听。比较重要。方法在下面。
+                .setOnBannerListener(this)
+                //必须最后调用的方法，启动轮播图。
+                .start();
+    }
+
+    //轮播图的监听方法
+    @Override
+    public void OnBannerClick(int position) {
+        Log.i("tag", "你点了第"+position+"张轮播图");
+        Toast.makeText(this.getContext(),"你点了第"+position+"张轮播图",Toast.LENGTH_SHORT).show();
+    }
+
+    //自定义的图片加载器
+    private class MyLoader extends ImageLoader {
+        @Override
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            Glide.with(context).load((String) path).into(imageView);
+        }
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
